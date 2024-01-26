dd9d04f755b605c3fb71d566293d54e57765372d
baljang <baljang7@gmail.com>
2024-01-26 11:38:22 +0900

    엔진구조_Material
    1. Mesh
        1) MeshRenderer.h의 // Mesh 부분을 Mesh.h로 옮긴다.
        2) void Mesh::CreateDefaultRectangle()은 MeshRenderer 생성자에서 만들어 준 부분을 가져온다.
        3) MeshRenderer.h의 // Mesh 가 있던 자리에 shared_ptr<Mesh> _mesh;선언하기
        4) MeshRenderer.h SetMesh, GetMesh를 만들고 활용해 meshRenderer에 있었던 _vertexBuffer, _indexBuffer의 빈자리 채우기
        5) ResourceManager::CreateDefaultMesh()에서 mesh를 만들고, _resources에 넣기
        6) SceneManager::LoadTestScene()에서 mesh를 RESOURCES로 부터 Get 하여 SetMesh 해주기
    
    2. Material
        1) MeshRenderer의 //Material 부분을 Shader.h로 옮기고, Shader를 ResourceManager의 friend로 하기
        2) Shader와 Texture들고 있게 Material.h 채우기
        3) ResourceManager::CreateDefaultShader로 MeshRenderer의 생성자에서 _vertexShader, _inputLayout, _pixelShader를 옮기고 shader에 세팅해서 _resources에 넣기
        4) ResourceManager::CreateDefaultMaterial에서 Shader와 Texture를 세팅한 material을 _resources에 넣기
        5) MeshRenderer에서 필요 없는 부분을 정리하고, shared_ptr <Material> _material 변수를 선언하고, material을 활용할 헬퍼 함수들을 만들기
        6) SceneManager::LoadTestScene()에서 _resources에서 Material을 Get 해서 meshRenderer에 set 하기
        7) RenderManager::RenderObjects()에서 MeshRenderer의 헬퍼 함수들을 통해 _mesh, _material에 세팅된 요소들 활용하게 하기

--- GameCoding/GameCoding/MeshRenderer.cpp
+++ GameCoding/GameCoding/MeshRenderer.cpp
@@ -3,42 +3,16 @@
 #include "Camera.h"
 #include "Game.h"
 #include "Pipeline.h"
+#include "Mesh.h"
 
 MeshRenderer::MeshRenderer(ComPtr<ID3D11Device> device, ComPtr<ID3D11DeviceContext> deviceContext)
 	: Super(ComponentType::MeshRenderer), _device(device)
 {
-	_geometry = make_shared<Geometry<VertexTextureData>>();
-	GeometryHelper::CreateRectangle(_geometry);
-	// _geometry = make_shared<Geometry<VertexColorData>>();
-	//GeometryHelper::CreateRectangle(_geometry, Color{1.0f, 0.f, 0.f, 1.0f});
-
-	_vertexBuffer = make_shared<VertexBuffer>(device);
-	_vertexBuffer->Create(_geometry->GetVertices());
-
-	_indexBuffer = make_shared<IndexBuffer>(device);
-	_indexBuffer->Create(_geometry->GetIndices());
-
-	_vertexShader = make_shared<VertexShader>(device);
-	_vertexShader->Create(L"Default.hlsl", "VS", "vs_5_0");
-
-	_inputLayout = make_shared<InputLayout>(device);
-	_inputLayout->Create(VertexTextureData::descs, _vertexShader->GetBlob());
-	//_inputLayout->Create(VertexColorData::descs, _vertexShader->GetBlob());
-
-	_pixelShader = make_shared<PixelShader>(device);
-	_pixelShader->Create(L"Default.hlsl", "PS", "ps_5_0");
-
-	_texture1 = make_shared<Texture>(device);
-	_texture1->Create(L"chiikawa.png");
 }
 
 MeshRenderer::~MeshRenderer()
 {
 }
 
-void MeshRenderer::Update()
-{
-	// Render(GGame->GetPipeline());
-}
 
 
--- GameCoding/GameCoding/ResourceManager.cpp
+++ GameCoding/GameCoding/ResourceManager.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "ResourceManager.h"
 #include "Texture.h"
+#include "Mesh.h"
+#include "Shader.h"
+#include "Material.h"
+#include "Animation.h"
 
 ResourceManager::ResourceManager(ComPtr<ID3D11Device> device)
 	: _device(device)
@@ -29,14 +33,38 @@ void ResourceManager::CreateDefaultTexture()
 
 void ResourceManager::CreateDefaultMesh()
 {
+	// Mesh
+	shared_ptr<Mesh> mesh = make_shared<Mesh>(_device); 
+	mesh->SetName(L"Rectangle"); 
+	mesh->CreateDefaultRectangle(); 
+	Add(mesh->GetName(), mesh); 
 }
 
 void ResourceManager::CreateDefaultShader()
 {
+	auto vertexShader = make_shared<VertexShader>(_device);
+	vertexShader->Create(L"Default.hlsl", "VS", "vs_5_0");
+	auto inputLayout = make_shared<InputLayout>(_device);
+	inputLayout->Create(VertexTextureData::descs, vertexShader->GetBlob());
+	auto pixelShader = make_shared<PixelShader>(_device);
+	pixelShader->Create(L"Default.hlsl", "PS", "ps_5_0");
+
+	// Shader
+	shared_ptr<Shader> shader = make_shared<Shader>(); 
+	shader->SetName(L"Default");
+	shader->_vertexShader = vertexShader; 
+	shader->_inputLayout = inputLayout; 
+	shader->_pixelShader = pixelShader; 
+	Add(shader->GetName(), shader); 
 }
 
 void ResourceManager::CreateDefaultMaterial()
 {
+	shared_ptr<Material> material = make_shared<Material>(); 
+	material->SetName(L"Default"); 
+	material->SetShader(Get<Shader>(L"Default"));
+	material->SetTexture(Get<Texture>(L"chiikawa"));
+	Add(material->GetName(), material); 
 }
 
 void ResourceManager::CreateDefaultAnimation()
